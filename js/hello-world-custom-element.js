58a7963624d228511a8d1a770520d106895955ba
Katja Bregenzer <katja.bregenzer@lime.tech>
2019-01-21 11:42:14 +0100

    section custom elements

---
+++ js/hello-world-custom-element.js
@@ -0,0 +1,22 @@
+class HelloWorldCustomElement extends HTMLElement{
+
+    // Store properties as attributes so they work both ways.
+    get name() {
+        return this.getAttribute('name');
+    }
+
+    set name(name) {
+        this.setAttribute('name', name);
+    }
+
+
+    connectedCallback() {
+        this._content = document.createElement('span');
+        this.name = this.name || 'World';
+        this._content.innerText = 'Hello ' + this.name;
+        this.appendChild(this._content);
+    }
+}
+
+// Registers <lth-hello-world> as a custom element
+window.customElements.define('lth-hello-world', HelloWorldCustomElement);
